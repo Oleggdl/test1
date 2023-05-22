a677183e517abf20c72e702aca8dea3cce2e63aa
Oleg <oblotskiyj@gmail.com>
2023-05-22 22:34:23 +0300

    commit 4

---
+++ src/pages/[id].tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Article = (): JSX.Element => {
+
+  return (
+    <>
+      <Head>
+        <meta name="description" content="test another page 22:23" />
+      </Head>
+      <div>
+        test another page
+      </div>
+    </>
+  );
+};
+
+export default Article;
