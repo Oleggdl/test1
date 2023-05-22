0f5e8451cb19bf6eea6ed43a705db920c9fffafb
Oleg <oblotskiyj@gmail.com>
2023-05-22 22:23:46 +0300

    commit 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Dashboard = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <meta name="description" content={'delpoy 3'} />
+      </Head>
+      <div>
+        test descriprion 22:22
+      </div>
+    </>
+  );
+};
+export default Dashboard;
