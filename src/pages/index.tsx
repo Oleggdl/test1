b5c6f58692e3858d735895a02e144c52defdf5cc
Oleg <oblotskiyj@gmail.com>
2023-05-22 22:30:28 +0300

    commit 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ const Dashboard = (): JSX.Element => {
   return (
     <>
       <Head>
-        <meta name="description" content={'delpoy 3'} />
+        <meta name="description" content="{'delpoy 3'}" />
+        <title>test 22:29</title>
       </Head>
       <div>
-        test descriprion 22:22
+        test descriprion 22:30
       </div>
     </>
   );
