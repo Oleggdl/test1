9287d901a21f5faf7bc30563cacb87d5e663f13f
Oleg <oblotskiyj@gmail.com>
2023-05-22 23:41:35 +0300

    commit 18

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({Component, pageProps}: AppProps) {
     return <>
         <Head>
             <title>Atlant Blog app</title>
+            <meta name="description" content="test another page 23:40" />
         </Head>
         <Component {...pageProps} />
     </>;
