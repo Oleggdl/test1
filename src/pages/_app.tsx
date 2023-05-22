a8aae145e683c496d1c2b94e1ccfa46f246d931d
Oleg <oblotskiyj@gmail.com>
2023-05-22 23:26:58 +0300

    commit 16

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    return <>
+        <Head>
+            <title>Atlant Blog app</title>
+        </Head>
+        <Component {...pageProps} />
+    </>;
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <title>Atlant Blog document</title>
+                </Head>
                 <body>
                 <Main />
                 <NextScript />
