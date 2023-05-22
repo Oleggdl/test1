1003b0dcef37020d1f504c39c1ecbb6689f19933
Oleg <oblotskiyj@gmail.com>
2023-05-22 22:48:26 +0300

    commit 7

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,22 @@
 import Head from 'next/head';
+import type {AppProps} from 'next/app';
 
-function App() {
+function App({Component, pageProps}: AppProps) {
     return (
         <>
-                <Head>
-                    <title>Atlant Blog</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    <link rel="manifest" href="/site.webmanifest" />
-                    <meta name="msapplication-TileColor" content="#da532c"></meta>
-                    <meta name="theme-color" content="#ffffff" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <meta name="description" content={'app.descriptionMainPage'} />
-                </Head>
-               <div>
-                   _app 22:43
-               </div>
+            <Head>
+                <title>Atlant Blog</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <meta name="msapplication-TileColor" content="#da532c"></meta>
+                <meta name="theme-color" content="#ffffff"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="description" content={'app.descriptionMainPage'}/>
+            </Head>
+            <Component {...pageProps} />
         </>
     );
 }
